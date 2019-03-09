0deb9301b0b28ff5ba697830029bb1e5d17ce062
Ju An Oh <juan@Jus-MacBook-Pro.local>
2019-03-09 12:16:31 -0800

    firebase set up

---
+++ ranker/ranker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ranker
+//
+//  Created by Ju An Oh on 3/9/19.
+//  Copyright © 2019 huskyluverzz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
