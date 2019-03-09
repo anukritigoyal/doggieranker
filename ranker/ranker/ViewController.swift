81bc5e3960b0dfea231c383537ff4cae69d2740b
Anukriti Goyal <anukritigoyal@D-10-19-125-50.dhcp4.washington.edu>
2019-03-09 15:53:18 -0800

    home page additions

--- ranker/ranker/ViewController.swift
+++ ranker/ranker/ViewController.swift
@@ -10,9 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ranker: UIButton!
+    @IBOutlet weak var game: UIButton!
+    @IBOutlet weak var submit: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(red: 0.98, green: 0.75, blue: 0.18, alpha: 1)
+        
     }
 
 
