4f1d27fff47689ab57703c74bd31348d33b966e8
Student User <studentuser@D-10-18-241-171.dhcp4.washington.edu>
2019-03-09 16:09:34 -0800

    mock view finnished

--- ranker/ranker/GuessingGameViewController.swift
+++ ranker/ranker/GuessingGameViewController.swift
@@ -2,17 +2,43 @@
 //  GuessingGameViewController.swift
 //  ranker
 //
-//  Created by Student User on 3/9/19.
+//  Created by Matthew Vogt on 3/9/19.
 //  Copyright © 2019 huskyluverzz. All rights reserved.
 //
 
+/*
+ Core:
+ 
+    First the view should load with nothing inside of it besides instructions on how to play the game.
+    Maybe have something to signify that the game is loading or downloading the images
+    When images are finnished loading, show the button to display the game
+ 
+    A game will consist of two images, when one is clicked  they are either right or wrong
+    ???Give them a confirm option???
+        right: Tell them they are correct, option to quit / see leaderboard, next round
+        wrong: Tell them they are wrong, they can see leaderboard or quit from here.
+ 
+*/
 import UIKit
 
 class GuessingGameViewController: UIViewController {
     
+    @IBOutlet weak var gameTitle: UILabel!
+    @IBOutlet weak var gameDesc: UILabel!
+    
+    @IBOutlet weak var imageStackView: UIStackView!
+    
+    @IBOutlet weak var startGameButton: UIButton!
+    @IBOutlet weak var loadingStackView: UIStackView!
+    
+    @IBOutlet weak var leaderboard: UIButton!
     
     @IBOutlet weak var firstDog: UIImageView!
     @IBOutlet weak var secondDog: UIImageView!
+    @IBOutlet weak var correctGuessStack: UIStackView!
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    var correctGuesses = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,19 +48,51 @@ class GuessingGameViewController: UIViewController {
         let singleTapDog2 = UITapGestureRecognizer(target: self, action: #selector(secondDogSelected))
         secondDog.isUserInteractionEnabled = true
         secondDog.addGestureRecognizer(singleTapDog2)
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // fetch images... This currently is mimicing the delay that the fetch would take
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) { // Change `2.0` to the desired number of seconds.
+            self.loadingStackView.isHidden = true
+            
+            self.startGameButton.isHidden = false
+        }
+        
     }
     
 
-
+    @IBAction func startGame(_ sender: UIButton) {
+        self.startGameButton.isHidden = true
+        self.gameTitle.isHidden = true
+        self.gameDesc.isHidden = true
+        self.leaderboard.isHidden = true
+        self.imageStackView.isHidden = false
+    }
     
-    //Action
+    
     @objc func firstDogSelected() {
-        print("Dog 1 clicked")
+        dogSelected(dog: 1)
     }
     
     @objc func secondDogSelected() {
-        print("Dog 2 clicked")
+        dogSelected(dog: 2)
     }
     
+    // Displays the step after a picture is selected
+    //   if user is correct: display their new score and ask them to go to the next page.
+    //   else: take them to the next page or give them the option to go to the leaderboard?
+    //
+    // dog represents the picture number(1 or 2) that the user selected
+    func dogSelected(dog: Int) {
+        if dog == 1 {
+            self.correctGuesses += 1
+            self.scoreLabel.text = "Score: \(self.correctGuesses)"
+            self.correctGuessStack.isHidden = false
+        } else {
+            
+        }
+    }
+    
+    @IBAction func nextPair(_ sender: UIButton) {
+        self.correctGuessStack.isHidden = true
+        //
+    }
 }
---
+++ ranker/ranker/InputScoreViewController.swift
@@ -0,0 +1,14 @@
+//
+//  InputScoreViewController.swift
+//  ranker
+//
+//  Created by Matthew Vogt on 3/9/19.
+//  Copyright © 2019 huskyluverzz. All rights reserved.
+//
+
+import UIKit
+
+class InputScoreViewController: UIViewController {
+
+
+}
