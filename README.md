314f82c6c10ef8256a6efb54906466e0f1750546
Peiyu Yi <peiyuyi@D-10-19-54-167.dhcp4.washington.edu>
2019-03-16 20:25:56 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # doggieranker
+# doggieranker
---
+++ ranker/ranker/GuessingGameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GuessingGameViewController.swift
+//  ranker
+//
+//  Created by Peiyu Yi on 3/12/19.
+//  Copyright © 2019 huskyluverzz. All rights reserved.
+//
+
+import UIKit
+
+class GuessingGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
